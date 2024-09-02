942ae23974aa3f5fbace3eab813352ddcec90dd3
harmyJasoliya <jasoliyaharmy2024.katargam@gmail.com>
2024-09-02 09:12:15 +0530

    add

---
+++ src/new/New1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material'
+function New1() {
+  return (
+    <>
+        <Box sx={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <Box sx={{border:1,borderColor:'black',borderRadius:'50px'}}>
+
+            </Box>
+        </Box>
+
+    </>
+  )
+}
+
+export default New1
